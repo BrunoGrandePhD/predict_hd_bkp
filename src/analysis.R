35807d8315ea94157aef6fc98a93de5ff07212b4
Bruno Grande <bgrande@sfu.ca>
2017-07-12 09:48:02 -0700

    Add code for random forest

--- src/analysis.R
+++ src/analysis.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 
+
+# Load and tidy data ------------------------------------------------------
+
 raw_data <- read.csv("data/raw_data/Heart.csv")
 
 clean_data <- select(raw_data, -X)
@@ -7,3 +10,22 @@ clean_data <- select(raw_data, -X)
 clean_data <- drop_na(clean_data)
 
 write.csv(clean_data, "data/clean_data/heart.clean.csv")
+
+
+# Split data --------------------------------------------------------------
+
+train <- sample(1:nrow(heart), nrow(heart) * 0.7)
+
+test <- -train
+
+
+# Analyze data ------------------------------------------------------------
+
+fit <- randomForest(AHD ~ ., data = heart, subset = train, importance = TRUE,
+                    xtest = heart[test, -ncol(heart)], ytest = heart[test, "AHD"])
+
+print(fit)
+
+plot(fit)
+
+varImpPlot(fit)
